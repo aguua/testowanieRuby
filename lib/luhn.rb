c3e01f74572b7367d876c6415c3ee5d9f9fad7ae
Agnieszka <aguua5@gmail.com>
2019-11-03 09:58:12 +0100

    remove luhn class and mini test

--- lib/luhn.rb
+++
@@ -1,8 +0,0 @@
-class Luhn
-    def self.valid? string
-      s = string.delete(' ')
-      return false if s.length <= 1 or /\D/.match? s
-  
-      s.chars.reverse.map.with_index {|x,i| (i+1) % 2 == 0 ? (x.to_i*2)>9?(x.to_i*2)-9:(x.to_i*2) : x.to_i }.sum % 10 == 0
-    end
-  end
