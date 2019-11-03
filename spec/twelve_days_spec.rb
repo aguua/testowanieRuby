3d3c860f40f6f203f6a1535fae1a321b01b632c2
Agnieszka <aguua5@gmail.com>
2019-11-03 21:48:39 +0100

    twelve days

--- spec/twelve_days_spec.rb
+++ spec/twelve_days_spec.rb
@@ -2,7 +2,7 @@ require 'twelve_days'
 
 RSpec.describe  TwelveDays do
   it("get the whole song") do
-    song_file = ".\\test\\song.txt"
+    song_file = "..\\test\\song.txt"
     expected = IO.read(song_file)
     expect(TwelveDays.song).to(eq(expected))
   end
