6a6bcbc0866f9839f231ab812f5d05780eb99038
Agnieszka <aguua5@gmail.com>
2019-11-04 12:42:18 +0100

    try simplecov

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,7 +1,8 @@
 # frozen_string_literal: true
 
 require 'simplecov'
-SimpleCov.start do
-  add_filter '/spec/' # for rspec
-  add_filter '/test/' # for minitest
-end
+SimpleCov.start 'rails'
+#SimpleCov.start do
+#  add_filter '/spec/' # for rspec
+#  add_filter '/test/' # for minitest
+#end
